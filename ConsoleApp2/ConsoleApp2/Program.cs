4e84eb5580924812fbda78ba04f58e453c8aca36
ArturBura <154540623+ArturBura@users.noreply.github.com>
2024-02-12 20:47:06 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задание 1
+//int[] mus = new int[6];
+
+//for(int y = 0; y <= 5; y++)
+//{
+//    Console.Write($"mus[{y}]=");
+//    mus[y] = int.Parse( Console.ReadLine() );
+//}
+//for (int y = 0; y <= mus.Length; y++)
+//{
+//    Console.WriteLine($"mus[{y}]={mus[y]}");
+//}
+//Console.ReadKey();
+// Задание 2
+//int[] omus = { 2, 3, 3, 4, 5 };
+//for (int i = 0; i <= omus.Length ;  i++)
+//{
+//    Console.WriteLine($"omus[{i}]={omus[i]}");
+//}
+//Задание 3
+//int[] omus = { 1, 2, 3, 4, 5 };
+//foreach (int i in omus)
+//{
+//    Console.WriteLine(i);
+//}
+// Задание 4
+//Random random = new Random();
+//int[] numbers = new int[6];
+//numbers[0]=random.Next(0, 10);
+//numbers[1] = random.Next(0, 10);
+//numbers[2] = random.Next(0, 10);
+//numbers[3] = random.Next(0, 10);
+//numbers[4] = random.Next(0, 10);
+//numbers[5] = random.Next(0, 10);
+//foreach (int i in numbers)
+//{
+//    Console.WriteLine(i);
+//}
+
